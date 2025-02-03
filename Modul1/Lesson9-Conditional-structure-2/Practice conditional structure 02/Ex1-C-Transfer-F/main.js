b4e3c5e15f8253e2089ac21b09e619519803af12
tiendoan2005 <tiendoan20005@gmail.com>
2025-02-03 19:35:02 +0700

    Update

--- Modul1/Lesson9-Conditional-structure-2/Practice conditional structure 02/Ex1-C-Transfer-F/main.js
+++
@@ -1,5 +0,0 @@
-function Check() {
-    let num = parseInt(document.getElementById("Done").value);
-    let resultt = (num * (9 / 5)) + 32;
-    document.getElementById("Kq").innerHTML = resultt + " F";
-}
